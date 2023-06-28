547cc13a5739193e03367fcafb7c5294c41c9a36
Samuel Calderon <samuel.calderon@uarm.pe>
2023-06-28 12:17:30 -0500

    trying the magic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import 'bootstrap/dist/css/bootstrap.min.css';
+    import InputSelect from './components/InputSelect.svelte';
+
+    let btnStyles = [
+        "btn-primary",
+        "btn-secondary",
+        "btn-danger",
+        "btn-warning"
+    ]
+</script>
+
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-4 col-12 bg-primary">Hello</div>
+        <div class="col-md-8 col-12 bg-secondary">Bye</div>
+    </div>
+    {#each btnStyles as btnStyle}
+        <InputSelect {btnStyle}/>
+    {/each}
+</div>
+
---
+++ src/routes/components/InputSelect.svelte
@@ -0,0 +1,12 @@
+<script>
+    // import 'bootstrap/dist/css/bootstrap.min.css';
+
+    export let btnStyle = "btn-primary"
+
+    let clicks = 0;
+    const counter = () => {
+        clicks = clicks + 1
+    }
+</script>
+
+<button class="btn {btnStyle}" on:click={counter}>Clicked {clicks} times</button>
