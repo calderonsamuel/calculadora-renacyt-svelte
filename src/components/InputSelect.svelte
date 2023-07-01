45d2c056d9e8845b0ad8a441d151f12b2918ac2c
calderonsamuel <samuel.calderon@uarm.pe>
2023-07-01 16:41:37 -0500

    use binding

--- src/components/InputSelect.svelte
+++ src/components/InputSelect.svelte
@@ -1,27 +1,15 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-
-
     export let id = "select-input-id";
     export let label = "";
     export let options = [{value: "", label: ""}];
 
-    let selected = "";
+    let selected: string;
     
-    const updateSelected = () => {
-        const element = document.getElementById(id) as HTMLInputElement;
-        selected = element?.value;
-    }
-
-    onMount(updateSelected)
-
-
-
 </script>
 
 <div class="">
     <label for="{id}" class="me-1">{label}</label>
-    <select {id} class="form-select me-2" aria-label="Default select example" on:change={updateSelected}>
+    <select {id} class="form-select me-2" aria-label="Default select example" bind:value={selected}>
         {#each options as el}
             <option value="{el.value}">{el.label}</option>
         {/each}
