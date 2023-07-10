bc476290788cf6362518e4d6efb07fa89a703a9f
Samuel Calderon <samuel.calderon@uarm.pe>
2023-07-10 16:16:13 -0500

    add meta description

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <svelte:head>
     <title>Calculadora Renacyt</title>
+    <meta name="description" content="Calculadora de puntajes RENACYT. Elaborada por Samuel Calderon usando SvelteKit.">
 
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
